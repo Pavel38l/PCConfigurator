941ef77dcc62f5be66034c1719481b5c3441cd88
Burdyug Pavel <serhioburdug@gmail.com>
2020-12-23 18:35:25 +0300

    Spring boot and dependencies

---
+++ backend/src/main/java/ru/vsu/Peredachka/PeredachkaApplication.java
@@ -0,0 +1,13 @@
+package ru.vsu.Peredachka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeredachkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeredachkaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ru/vsu/Peredachka/PeredachkaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vsu.Peredachka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeredachkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
