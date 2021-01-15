f1664537d3ede033a342adbc2318119b180d2076
Burdyug Pavel <serhioburdug@gmail.com>
2021-01-15 17:43:59 +0300

    Test jenkins

---
+++ backend/src/test/java/ru/vsu/Peredachka/service/UserServiceTest.java
@@ -0,0 +1,18 @@
+package ru.vsu.Peredachka.service;
+
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
+import ru.vsu.Peredachka.data.entity.User;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+    @Test
+    void getAllUsers() {
+        Assertions.assertEquals(2, 2);
+        //Assertions.assertEquals(4, userService.getAllUsers().size());
+    }
+}
