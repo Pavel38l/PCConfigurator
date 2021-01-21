97d4978944bceeffe6a3342cdf1b008574ce125d
Burdyug Pavel <serhioburdug@gmail.com>
2021-01-21 18:56:53 +0300

    Port changed to 8080 back

--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const USERS_REST_API_URL = "http://localhost:8081/api/v1/user";
+const USERS_REST_API_URL = "http://localhost:8080/api/v1/user";
 
 class UserService {
 
