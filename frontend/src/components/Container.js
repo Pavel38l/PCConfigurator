01d6828564e24676f1dc58d8c2926f3ac6f799ef
Daniil Demchenko <danyadw2000@gmail.com>
2021-04-16 18:06:28 +0300

    обнавлен профиль

--- frontend/src/components/Container.js
+++ frontend/src/components/Container.js
@@ -2,6 +2,6 @@ import { Card } from "antd";
 import React from "react";
 
 const PageContainer = ({ children }) => {
-  return <Card style={{ margin: 10 }}>{children}</Card>;
+  return <Card style={{ margin: 10 }} bordered={false}>{children}</Card>;
 };
 export default PageContainer;
